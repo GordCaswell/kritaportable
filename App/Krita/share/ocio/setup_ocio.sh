66a5f0b6ef2c5f46ea2218fce7a21510ae722362
Gord Caswell <GordCaswell@users.noreply.github.com>
2016-09-13 21:09:15 -0500

    Copy in current official release (3.0) & update .gitignore

--- App/Krita/share/ocio/setup_ocio.sh
+++ App/Krita/share/ocio/setup_ocio.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-OCIO_ROOT="/dev/desktop32/i"
-OCIO_EXECROOT="/dev/desktop32/i"
+OCIO_ROOT="/data2/cross/mxe/usr/i686-w64-mingw32.shared"
+OCIO_EXECROOT="/data2/cross/mxe/usr/i686-w64-mingw32.shared"
 
 # For OS X
 export DYLD_LIBRARY_PATH="${OCIO_EXECROOT}/lib:${DYLD_LIBRARY_PATH}"
